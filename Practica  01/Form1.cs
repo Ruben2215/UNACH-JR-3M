34b585c922702752c233b22b462d2eba4d48daf9
Jose Clemente <jose_clemente_corzo@hotmail.com>
2024-08-10 20:19:25 -0600

    Esta es una prueba de cambios

--- Practica  01/Form1.cs
+++ Practica  01/Form1.cs
@@ -16,5 +16,10 @@ namespace Practica__01
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ES UN COMENTARIO DE PRUEBA DE CAMBIOS 
+        }
     }
 }
